cb2b76a95811637bc12874aeed958ae31b09ff7e
Prajal <prajalbhattarai1627@gmail.com>
2025-03-10 21:18:09 +0545

    added even listener

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -1,3 +1,9 @@
-function myFunction () {
+function myFunction() {
     document.getElementById("demo").innerHTML = "paragraph changed to dinesh chutiya"
 }
+
+console.log("hello prajal");
+
+document.getElementById("alert").addEventListener('click', function () {
+    alert("Alert!! Someone clicked this");
+});
