94df016fa845ce9e1ecae65b493597006f233aa0
Prajal <prajalbhattarai1627@gmail.com>
2025-03-17 22:23:13 +0545

    question 3

--- JAVASCRIPT/labQuestions.js
+++ JAVASCRIPT/labQuestions.js
@@ -1,2 +1,17 @@
 //welcome to js
 console.log("welcome to JS");
+
+// question 2
+let num = 10;
+let name = "prajal";
+var n = 12;
+let bool = false;
+let und = undefined;  // Use `undefined` instead of an empty string
+let nulla = null;
+
+console.log(typeof num);
+console.log(typeof name); 
+console.log(typeof n);
+console.log(typeof bool);
+console.log(typeof und);
+console.log(typeof nulla);
