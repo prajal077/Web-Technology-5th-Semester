ef5bc3a49e48505ccd66794ebe20264b1db66ee1
Prajal <prajalbhattarai1627@gmail.com>
2025-03-19 00:51:34 +0545

    creating nested object and accessing the value of it

--- JAVASCRIPT/objectsInJs.js
+++ JAVASCRIPT/objectsInJs.js
@@ -1,15 +1,15 @@
 //creating objects in js
 
 const person = {
-    name : "prajal", 
-    age : 20
+    name: "prajal",
+    age: 20
 };
 
 console.log(person)
 
 // or we can also do in single line
 
-const std = { name : "aalu", age : 22}
+const std = { name: "aalu", age: 22 }
 console.log(std);
 
 // accessing the value of abjects
@@ -27,9 +27,9 @@ console.log(std)
 // 1. replacing the value of existing key
 
 let obj = {
-    name : "object", 
-    age : "2 seconds", 
-    hobby : "singing"
+    name: "object",
+    age: "2 seconds",
+    hobby: "singing"
 };
 
 console.log(obj)
@@ -45,11 +45,26 @@ console.log(obj)
 //js objects method
 
 let bob = {
-    name : "bob", 
-    age : 19, 
+    name: "bob",
+    age: 19,
 
-    greet : function () {
+    greet: function () {
         console.log("bob says hi");
     }
 }
 bob.greet();
+
+// nested object
+
+const per = {
+    name: "praj",
+    class: "15", 
+
+    marks : {
+        science : 10, 
+        math : 20
+    }
+}
+console.log(per)
+
+console.log(per.marks.math) // accessing the value of nested object
